ba94ba1dec6d3e4f4c28bb5c47f9b5b696fcf96a
Nevil Joyice <129611420+neviljo@users.noreply.github.com>
2024-03-04 16:09:43 +0000

    01 basics

--- 01basics/test.js
+++ 01basics/test.js
@@ -1,12 +1,4 @@
 
-let ball="pen"
-let a=23
-let b=true
-let c
-let d=null
-console.log(typeof ball)
-console.log(typeof b)
-console.log(typeof a)
-console.log(typeof c)
-console.log(typeof d)
+console.log("2"+3+3)
+console.log(2+3+"3")
 
